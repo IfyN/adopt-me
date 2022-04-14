8165f4fe8ba8545a8b978781161c56bb73e333a3
IfyN <ifynwosu0@gmail.com>
2022-04-14 06:42:16 +0100

    add selector hooks for select animal option

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,11 +1,18 @@
 import { useState } from "react";
 
+const Animals = ["bird", "cat", "dog", "parrot", "badger"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("");
 
   function updateLocation(e) {
     setLocation(e.target.value);
   }
+  function updateAnimal(e) {
+    setAnimal(e.target.value);
+  }
+
   return (
     <div className="search-params">
       <form>
@@ -17,6 +24,19 @@ const SearchParams = () => {
             onChange={updateLocation}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          <select id="animal" value={animal} onChange={updateAnimal}>
+            <option />
+            {
+              //because Animals is an array and we have to map through it to create an array
+              Animals.map((animal) => (
+                <option value={animal} key={animal}>
+                  {animal}
+                </option>
+              ))
+            }
+          </select>
         </label>
         <button>Submit</button>
       </form>
