c650106ec2798e86895b2d74f92a55301808f9a8
IfyN <ifynwosu0@gmail.com>
2022-05-03 10:24:31 +0100

    add search component

--- src/Pet.js
+++ src/Pet.js
@@ -1,5 +1,13 @@
 import react from "react";
 
-const Pet = () => {
-  return <div></div>;
+const Pet = (props) => {
+  return (
+    <div>
+      <h1> {props.name}</h1>
+      <h1> {props.animal}</h1>
+      <h1> {props.breed}</h1>
+    </div>
+  );
 };
+
+export default Pet;
